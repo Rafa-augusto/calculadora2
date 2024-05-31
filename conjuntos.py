ba763924cbe6156c736f476e76c25929b0259303
Rafael Augusto de Oliveira Souza <163347145+Rafa-augusto@users.noreply.github.com>
2024-05-31 15:30:56 -0300

    Update conjuntos.py

--- conjuntos.py
+++ conjuntos.py
@@ -1,5 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as mp
+from time import sleep
 def conjuntos_numericos():
     A = []
     B = []
@@ -18,7 +17,47 @@ def conjuntos_numericos():
         y = int(input("Digite o número para o subconjunto B: "))
         B.append(y)     
     while True:
-        print(f"A = {A} B = {B}\n-----Selecione uma opção-----\n1. Verificar se A é subconjunto de B\n2. A união com B\n3. A intersecção com B\n4. Diferença entre A e B")
-        "\n"
+        sleep(0.5)
+        print(end="\n")
+        print(f"A = {sorted(A)} B = {sorted(B)}\n-----Selecione uma opção-----\n1. Verificar se A é subconjunto próprio de B\n2. A união com B\n3. A intersecção com B\n4. Diferença entre A e B\n5.Sair")
+        print(end="\n")
         opcao = int(input("Sua opção: "))
+        if opcao<1 or opcao>5:
+            print("ERRO")
+            continue
+        elif opcao == 1:
+            if len(A) != len(B):
+                print("B não é subconjunto próprio de A.")
+                continue
+            else:
+                answer = []
+                for i in range(len(A)):
+                    if A[i] != B[i]:
+                        answer.append(1)
+                    else:
+                        answer.append(0)
+                if sum(answer) == 0:
+                    print("A é subconjunto próprio de B")
+                    continue
+                else:
+                    print("A não é um subconjunto próprio de B")
+                    continue
+        elif opcao == 2:
+            Uniao = []
+            Uniao.extend(A)
+            Uniao.extend(B)
+            cont = 0
+            while cont<len(Uniao):
+                if Uniao.count(Uniao[cont])>1:
+                    Uniao.remove(Uniao[cont])
+                cont+=1
+            print(f"A união entre A e B é igual a {sorted(Uniao)}")
+        elif opcao == 3:
+            inter = []
+            inter.append(A)
+            inter.append(B)
+            
+
+
+
 conjuntos_numericos()
